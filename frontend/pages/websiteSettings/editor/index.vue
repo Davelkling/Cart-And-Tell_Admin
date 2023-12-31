33e684dd6b99e66061106f535a056c49c0e50721
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-12-31 22:11:00 +0800

    added loading

--- frontend/pages/websiteSettings/editor/index.vue
+++ frontend/pages/websiteSettings/editor/index.vue
@@ -16,6 +16,7 @@ const dataLoaded = ref(false);
 const data = ref<CMS>({ type: "", html: "" });
 const editMode = ref(false);
 const editing = ref(false);
+const crudLoading = ref(false);
 let type = route.query.type;
 
 async function fetchData() {
@@ -56,6 +57,7 @@ onBeforeRouteUpdate((to, from) => {
     }
   }
   type = to.query.type;
+  crudLoading.value = false;
   editMode.value = false;
   configured.value = false;
   dataLoaded.value = false;
@@ -98,6 +100,7 @@ function handleFilePicker(callback: any) {
     };
   };
 }
+
 function cancelButtonHandler() {
   if (
     confirm(
@@ -111,6 +114,7 @@ function cancelButtonHandler() {
 }
 // Apply Changes Button Handler
 async function buttonHandler() {
+  crudLoading.value = true;
   if (configured.value) {
     await updateCMS();
   } else {
@@ -136,6 +140,7 @@ async function createCMS() {
     dataLoaded.value = false;
     editMode.value = false;
     editing.value = false;
+    crudLoading.value = false;
     fetchData();
     alert("Changes Applied");
   }
@@ -154,6 +159,7 @@ async function updateCMS() {
     alert(error.data.message);
   });
   if (result) {
+    crudLoading.value = false;
     alert("Update Changes Applied");
     editMode.value = false;
     dataLoaded.value = false;
@@ -226,10 +232,36 @@ async function updateCMS() {
             </div>
 
             <button
+              v-if="!crudLoading"
               @click="buttonHandler"
               class="w-full max-w-[1216px] mx-auto px-4 py-2 font-bold text-2xl border-2 rounded-md mt-5 border-[#5B8ED8] bg-[#5B8ED8] text-white hover:bg-white hover:text-black active:bg-[#5B8ED8] active:text-[#5B8ED8]"
             >
               Apply Changes
+            </button>
+            <button
+              v-else
+              disabled
+              type="button"
+              class="w-full max-w-[1216px] mx-auto px-4 py-2 font-bold text-2xl border-2 rounded-md mt-5 border-[#5B8ED8] bg-[#5B8ED8] text-white"
+            >
+              <svg
+                aria-hidden="true"
+                role="status"
+                class="inline w-4 h-4 me-3 text-white animate-spin"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="#E5E7EB"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentColor"
+                />
+              </svg>
+              Processing...
             </button>
           </div>
           <div
